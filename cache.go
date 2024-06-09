d13bc8250f75f16f1368f5d85d97bc27fabce062
Riyan Dhiman <Riyandhiman14@gmail.com>
2024-06-09 16:15:50 +0530

    Added time cache

--- cache.go
+++ cache.go
@@ -1,14 +1,17 @@
 package main;
 
 import (
-//	"fmt"
+	"fmt"
 	"sync"
 	"net/http"
+	"time"
 	"github.com/gin-gonic/gin"
 )
 
 type CacheEntry struct {
 	Value string
+	Expiration int64
+	Expiry bool
 };
 
 type Cache struct {
@@ -25,13 +28,27 @@ func Init() *Cache {
 func (c *Cache) set(key, value string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.data[key] = CacheEntry{Value: value}
+	expiration := time.Now().AddDate(1,0,0).UnixNano()
+	c.data[key] = CacheEntry{Value: value, Expiration: expiration, Expiry: false}
 }
 
-func (c *Cache) get(key string) string {
+func (c *Cache) setT(key, value string, duration time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	expiration := time.Now().Add(duration).UnixNano()
+	c.data[key] = CacheEntry{Value: value, Expiration: expiration, Expiry: true}
+}
+
+
+
+func (c *Cache) get(key string) (string, bool) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
-	return c.data[key].Value
+	entry, found := c.data[key]
+	if !found || (entry.Expiry && entry.Expiration < time.Now().UnixNano()) {
+		return "", false
+	}
+	return entry.Value, true
 }
 
 func main() {
@@ -46,17 +63,38 @@ func main() {
 	r.GET("/set", func(c *gin.Context) {
 		key := c.Query("key")
 		value := c.Query("value")
-		go cache.set(key, value)
+		durationstr, exists := c.GetQuery("duration")
+		if exists {
+			fmt.Println(durationstr, exists)
+			duration, err := time.ParseDuration(durationstr)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"message": err,
+				})
+				return
+			}
+			go cache.setT(key, value, duration)
+		} else {
+			go cache.set(key, value)
+		}
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Key updated in cache",
 		})
 	})
 	r.GET("/get", func(c *gin.Context) {
 		key := c.Query("key")
-		value := cache.get(key)
-		c.JSON(http.StatusOK, gin.H{
-			"value": value,
-		})
+		value, found := cache.get(key)
+		if !found {
+			c.JSON(http.StatusNotFound, gin.H{
+				"message": "key not present in cache",
+				"found": found,
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"value": value,
+				"found": found,
+			})
+		}
 	})
 	r.Run("0.0.0.0:5000")
 }
